879f13a7b2c1f8b5c3800ba66f8e1f24da16bab7
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-09-23 14:43:31 +0300

    fix api register

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -2,41 +2,54 @@ import { NextRequest, NextResponse } from 'next/server';
 import { api } from '../../api';
 import { cookies } from 'next/headers';
 import { parse } from 'cookie';
+import { AxiosError } from 'axios';
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const response = await api.post('/auth/register', body);
 
-  const cookieData = await cookies();
-  const setCookie = response.headers['set-cookie'];
+  try {
+    const response = await api.post('/auth/register', body);
 
-  if (setCookie) {
-    const cookiesArray = Array.isArray(setCookie) ? setCookie : [setCookie];
+    const cookieData = await cookies();
+    const setCookie = response.headers['set-cookie'];
 
-    for (const newCookieString of cookiesArray) {
-      const parsedCookie = parse(newCookieString);
-      const options = {
-        path: parsedCookie.Path,
-        maxAge: Number(parsedCookie['Max-Age']),
-        expires: parsedCookie.Expires
-          ? new Date(parsedCookie.Expires)
-          : undefined,
-        httpOnly: true,
-        secure: true,
-      };
+    if (setCookie) {
+      const cookiesArray = Array.isArray(setCookie) ? setCookie : [setCookie];
 
-      if (parsedCookie.accessToken) {
-        cookieData.set('accessToken', parsedCookie.accessToken, options);
-      }
-      if (parsedCookie.refreshToken) {
-        cookieData.set('refreshToken', parsedCookie.refreshToken, options);
+      for (const newCookieString of cookiesArray) {
+        const parsedCookie = parse(newCookieString);
+
+        const options = {
+          path: parsedCookie.Path,
+          maxAge: Number(parsedCookie['Max-Age']),
+          expires: parsedCookie.Expires
+            ? new Date(parsedCookie.Expires)
+            : undefined,
+          httpOnly: true,
+          secure: true,
+        };
+
+        if (parsedCookie.accessToken) {
+          cookieData.set('accessToken', parsedCookie.accessToken, options);
+        }
+        if (parsedCookie.refreshToken) {
+          cookieData.set('refreshToken', parsedCookie.refreshToken, options);
+        }
       }
     }
+
+    const { data, status } = response;
+
+    return NextResponse.json(data, { status });
+  } catch (error) {
+    const err = error as AxiosError;
+
+    if (err.response) {
+      return NextResponse.json(err.response.data, {
+        status: err.response.status,
+      });
+    }
+
+    return NextResponse.json({ message: 'Щось пішло не так' }, { status: 500 });
   }
-
-  const { data } = response;
-
-  if (data) return NextResponse.json(data);
-
-  return NextResponse.json({ status: '500', message: 'error' });
 }
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -26,7 +26,7 @@ export interface LoginRequest {
 }
 
 const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+  baseURL: 'http://localhost:3000/api',
   withCredentials: true,
 });
 
