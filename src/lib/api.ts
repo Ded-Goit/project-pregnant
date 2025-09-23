f196b083c16b5bdfa7f1fb3c399f5d2b919421e4
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-09-23 19:12:52 +0300

    fix base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -26,7 +26,7 @@ export interface LoginRequest {
 }
 
 const nextServer = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
