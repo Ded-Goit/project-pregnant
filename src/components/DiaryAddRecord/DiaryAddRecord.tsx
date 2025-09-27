d5999eab33d7abf50556e9e2e1603873ef50b3c5
Anastasiia <anastasiianikolaienko2004@gmail.com>
2025-09-27 14:55:01 +0200

    Diary_Modal

--- src/components/DiaryAddRecord/DiaryAddRecord.tsx
+++ src/components/DiaryAddRecord/DiaryAddRecord.tsx
@@ -1,12 +1,44 @@
+'use client';
+
 import css from './DiaryAddRecord.module.css';
 import { createPortal } from 'react-dom';
+import { createDiary } from '@/lib/clientApi';
+import React, { useEffect, useState } from 'react';
+import { getEmotions } from '@/lib/clientApi';
+import type { CreateDiaryRequest } from '@/lib/clientApi';
+import type { Emotion } from '@/lib/clientApi';
 
 interface ModalProps {
   onClose: () => void;
 }
 
 export default function DiaryAddRecord({ onClose }: ModalProps) {
-  return createPortal(
+  const [emotionsList, setEmotionsList] = useState<Emotion[]>([]);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const fd = new FormData(e.currentTarget);
+    const payload: CreateDiaryRequest = {
+      title: String(fd.get('title') ?? ''),
+      emotions: fd.getAll('emotions').map((v) => String(v)),
+      descr: String(fd.get('descr') ?? ''),
+    };
+    // createDiary(payload);
+  };
+
+const EmotionT = emotionsList.map(({_id, title}) => (
+  <option key={_id} value={_id}>{title}</option>
+));
+
+useEffect(() => {
+    const fetchData = async () => {
+      const emotions = await getEmotions();
+      setEmotionsList(emotions.data.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
     <div className={css.backdrop}>
       <div className={css.modal}>
         <button
@@ -16,11 +48,26 @@ export default function DiaryAddRecord({ onClose }: ModalProps) {
         >
           &times;
         </button>
-
-        <h2>Modal Title</h2>
-        <p>This is some content inside the modal.</p>
+        <h2>Новий запис</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Заголовок:</label>
+            <input type="text" name="Заголовок"></input>
+          </div>
+          <div>
+            <label>Теги:</label>
+            {/* <input type="chekbox" /> */}
+            <select name="Теги" multiselect>
+              {EmotionT}
+            </select>
+          </div>
+          <label>Запис:</label>
+          <div>
+            <textarea name="Запис"></textarea>
+          </div>
+          <button type="submit">Зберегти</button>
+        </form>
       </div>
-    </div>,
-    document.body
+    </div>
   );
 }
