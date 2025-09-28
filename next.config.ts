1c813d6720220a86272c192ccaa6e5756de7f1b4
Serhii Zhelobkov <meeelnik@gmail.com>
2025-09-28 19:47:47 +0300

    last commit

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-<<<<<<< HEAD
     domains: ['ftp.goit.study'],
   },
   /* config options here */
-=======
-    remotePatterns: [{ protocol: 'https', hostname: 'res.cloudinary.com' }],
-  },
->>>>>>> main
+  remotePatterns: [{ protocol: 'https', hostname: 'res.cloudinary.com' }],
 };
 
 export default nextConfig;
