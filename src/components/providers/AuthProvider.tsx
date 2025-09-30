3857aab509aac820c9de379f34578c1cdab8f2bd
Nick <bogdanovichkolia1990@gmail.com>
2025-09-30 20:20:48 +0300

    fix provider

--- src/components/providers/AuthProvider.tsx
+++ src/components/providers/AuthProvider.tsx
@@ -3,30 +3,42 @@
 import { ReactNode, useEffect } from 'react';
 import { useAuthStore } from '@/hooks/useAuthStore';
 import { getMe, refresh } from '@/lib/clientApi';
+import Preloader from '../Preloader/Preloader';
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const setUser = useAuthStore((state) => state.setUser);
   const clearIsAuthenticated = useAuthStore(
     (state) => state.clearIsAuthenticated
   );
+  const setAuthLoading = useAuthStore((state) => state.setAuthLoading);
+  const isAuthLoading = useAuthStore((state) => state.isAuthLoading);
 
   useEffect(() => {
     const fetchUser = async () => {
-      const isAuth = await refresh();
+      setAuthLoading(true);
 
-      if (isAuth) {
-        const { user } = await getMe();
+      try {
+        const isAuth = await refresh();
 
-        if (user) {
-          setUser(user);
+        if (isAuth) {
+          const { user } = await getMe();
+          if (user) {
+            setUser(user);
+          }
+        } else {
+          clearIsAuthenticated();
         }
-      } else {
-        clearIsAuthenticated();
+      } finally {
+        setAuthLoading(false);
       }
     };
 
     fetchUser();
-  }, [clearIsAuthenticated, setUser]);
+  }, [setUser, clearIsAuthenticated, setAuthLoading]);
+
+  if (isAuthLoading) {
+    return <Preloader />;
+  }
 
   return <>{children}</>;
 }
