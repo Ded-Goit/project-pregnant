1dec6a450af1c15ff6d6f2882842a2035da0ba94
Nick <bogdanovichkolia1990@gmail.com>
2025-10-01 13:06:32 +0300

    colors

---
+++ src/app/api/users/updateUserData/[id]/route.ts
@@ -0,0 +1,36 @@
+// import { NextRequest, NextResponse } from 'next/server';
+// import { api } from '../../../api';
+// import { cookies } from 'next/headers';
+// import { AxiosError } from 'axios';
+
+// type Props = {
+//   params: Promise<{ id: string }>;
+// };
+
+// export async function PATCH(request: NextRequest, { params }: Props) {
+//   const { id } = await params;
+//   const body = await request.json();
+
+//   //   const cookieData = await cookies();
+//   //   const accessToken = cookieData.get('accessToken')?.value;
+
+//     try {
+//       const { data } = await api.patch(`/diaries/${id}`, body, {
+//         headers: { Authorization: `Bearer ${accessToken}` },
+//       });
+//       const { data } = await api.patch(`/diaries/${id}`, body, {
+//         headers: { Authorization: `Bearer ${accessToken}` },
+//       });
+
+//   //     return NextResponse.json(data);
+//   //   } catch (error) {
+//   //     const err = error as AxiosError;
+
+//   //     if (err.response) {
+//   //       return NextResponse.json(err.response.data, {
+//   //         status: err.response.status,
+//   //       });
+//   //     }
+//   return NextResponse.json({ message: 'Щось пішло не так' }, { status: 500 });
+//   //   }
+// }
