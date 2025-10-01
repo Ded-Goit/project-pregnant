70ae81163e7adf882bbc07978184c7682414cd3b
Nick <bogdanovichkolia1990@gmail.com>
2025-10-01 20:46:32 +0300

    add loader on edit profile

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import SpinnerFlowersLine from "@/components/SpinnerFlowersLine/SpinnerFlowersLine";
+
+const Loading = () => {
+    return <SpinnerFlowersLine></SpinnerFlowersLine>
+}
+
+export default Loading;
